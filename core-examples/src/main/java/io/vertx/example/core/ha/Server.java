b9d7f2ef6a49c5846199d1384662942fd619f527
vandriot <victor.andriot@outlook.com>
2017-11-23 10:58:04 +0100

    first commit

---
+++ core-examples/src/main/java/io/vertx/example/core/ha/Server.java
@@ -0,0 +1,63 @@
+package io.vertx.example.core.ha;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Launcher;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rabbitmq.RabbitMQClient;
+import io.vertx.rabbitmq.RabbitMQOptions;
+
+import java.lang.management.ManagementFactory;
+
+/*
+ * This is just a simple verticle creating a HTTP server. The served response contains an id identifying the process
+ * for illustration purpose as it will change when the verticle is migrated.
+ *
+ * The verticle is intended to be launched using the `-ha` option.
+ */
+public class Server extends AbstractVerticle {
+
+    // Convenience method so you can run it in your IDE
+    public static void main(String[] args) {
+
+        Launcher.main(new String[]{"run", Server.class.getName(), "-ha"});
+    }
+
+    @Override
+    public void start() throws Exception {
+        String QUEUE_NAME = "task_queue";
+
+        // PARAMETRE DE CONNEXION RABBITMQ
+        RabbitMQOptions config = new RabbitMQOptions();
+        // Each parameter is optional
+        // The default parameter with be used if the parameter is not set
+        config.setUser("test");
+        config.setPassword("test");
+        config.setHost("192.168.1.40"); // adresse IP du cluster rabbitMQ
+        config.setPort(5672);
+        config.setConnectionTimeout(6000); // in milliseconds
+        config.setRequestedHeartbeat(60); // in seconds
+        config.setHandshakeTimeout(6000); // in milliseconds
+        config.setRequestedChannelMax(5);
+        config.setNetworkRecoveryInterval(500); // in milliseconds
+        config.setAutomaticRecoveryEnabled(true);
+        RabbitMQClient client = RabbitMQClient.create(vertx, config);
+
+        //vertx.createHttpServer().requestHandler(req -> {
+            client.start(v-> {
+                vertx.setPeriodic(100, id -> { // Toutes les 100 miliisecondes
+                    client.basicGet(QUEUE_NAME, true, getResult -> {
+                        if (getResult.succeeded()) {
+                            JsonObject msg = getResult.result();
+                            String response = msg.getString("body");
+                            System.out.println("Got Message : "+ response);
+                            //req.response().end(response);
+                        } else {
+                            getResult.cause().printStackTrace();
+                        }
+                    });
+                });
+            });
+        //}).listen(8080);
+    }
+}
